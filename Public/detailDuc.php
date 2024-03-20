be8e80cbe824bb2e37852a8ae6e743f6b1ae30d9
nguyenhoaithanh <nguyenhoaithanh.05042002@gmail.com>
2024-03-20 14:37:28 +0700

    chèn ảnh vào detail du học

--- Public/detailDuc.php
+++ Public/detailDuc.php
@@ -20,13 +20,52 @@ include("inc/top.php");
     }
 </style>
 
-<div class="text-center">
-    <h1 class="container align-items-center mt-5 m-5 p-5 ">Du Học Đức </h1>
-    <h1 class=" mt-2 m-1 p-5 ">-------- <i class="fa-solid fa-map-location-dot"></i></i> -------- </h1></h3>
+<div class=" container align-items-center mt-5 p-5">
+    <h1 class=" text-md-center mt-3 p-3"> Du Học Đức </h1>
+    <h1 class="text-md-center">-------- <i class="fa-solid fa-map-location-dot"></i></i> -------- </h1></h3>
 </div>
 
-    
-
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+<div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>
+  </div>
+  <center>
+    <div class="carousel-inner" style=" width:1200px; height: 567px">
+    <div class="carousel-item active" >
+      <img src="../image/duhoc/d0.jpg" class="d-block w-100"  alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../image/duhoc/d1.jpg" class="d-block w-100"  alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../image/duhoc/d2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../image/duhoc/d3.jpeg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../image/duhoc/d4.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../image/duhoc/d5.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+</center>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 <?php
 include("inc/bottom.php");
 ?>
--- Public/detailMy.php
+++ Public/detailMy.php
@@ -19,11 +19,54 @@ include("inc/top.php");
         font-family:'Times New Roman', Times, serif, Helvetica, sans-serif;
     }
 </style>
-<div class="text-center">
-    <h1 class="container align-items-center mt-5 m-3 p-5"> Du Học Mỹ </h1>
-    <h1 class=" mt-2 m-1 p-5 ">-------- <i class="fa-solid fa-map-location-dot"></i></i> -------- </h1></h3>
+
+<div class=" container align-items-center mt-3 p-5 ">
+    <h1 class="text-md-center  mt-5  p-3">Du Học Mỹ </h1>
+    <h1 class="text-md-center">-------- <i class="fa-solid fa-map-location-dot"></i></i> -------- </h1></h3>
 </div>
 
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+<div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5" aria-label="Slide 6"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6" aria-label="Slide 7"></button>
+  </div>
+<center>
+  <div class="carousel-inner"  style="width: 1200px; height: 567px">
+    <div class="carousel-item active">
+      <img src="../image/duhoc/m0.jpg" class="d-block w-100"  alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../image/duhoc/m1.jpg" class="d-block w-100"  alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../image/duhoc/m2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../image/duhoc/m3.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../image/duhoc/m4.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../image/duhoc/m5.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+</center>
+  
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
     
 <?php
 include("inc/bottom.php");
