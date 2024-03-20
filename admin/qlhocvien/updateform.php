eed45fb254c3489afb0ed95b6b26896686499415
thanhtuu911 <emtentu0911@gmail.com>
2024-03-20 17:12:39 +0700

    update admin/qllop

--- admin/qlhocvien/updateform.php
+++ admin/qlhocvien/updateform.php
@@ -1,46 +1,48 @@
 <?php include("../inc/top.php"); ?>
 
-<div class="container">
-        <h2>Sửa Học Viên</h2>
-        <form action="index.php?action=xulysua" method="POST" enctype="multipart/form-data">
-            <input type="hidden" name="txtid" value="<?php echo $hocvien['id']; ?>">
-            <div class="form-group">
-                <label for="txthoten">Họ và Tên:</label>
-                <input type="text" class="form-control" id="txthoten" name="txthoten" value="<?php echo $hocvien['hoten']; ?>">
-            </div>
-            <div class="form-group">
-                <label for="txtnamsinh">Năm Sinh:</label>
-                <input type="text" class="form-control" id="txtnamsinh" name="txtnamsinh" value="<?php echo $hocvien['namsinh']; ?>">
-            </div>
-            <div class="form-group">
-                <label for="txtgioitinh">Giới Tính:</label>
-                <input type="text" class="form-control" id="txtgioitinh" name="txtgioitinh" value="<?php echo $hocvien['gioitinh']; ?>">
-            </div>
-            <div class="form-group">
-                <label for="txtemail">Email:</label>
-                <input type="email" class="form-control" id="txtemail" name="txtemail" value="<?php echo $hocvien['email']; ?>">
-            </div>
-            <div class="form-group">
-                <label for="txtsdt">Số Điện Thoại:</label>
-                <input type="text" class="form-control" id="txtsdt" name="txtsdt" value="<?php echo $hocvien['sodienthoai']; ?>">
-            </div>
-            <div class="form-group">
-                <label for="txtdiachi">Địa Chỉ:</label>
-                <input type="text" class="form-control" id="txtdiachi" name="txtdiachi" value="<?php echo $hocvien['diachi']; ?>">
-            </div>
-            <div class="form-group">
-                <label for="filehinhanh">Hình Ảnh:</label>
-                <input type="file" class="form-control-file" id="filehinhanh" name="filehinhanh">
-            </div>
-            <input type="hidden" name="txthinhcu" value="<?php echo $hocvien['hinhanh']; ?>">
-            <button type="submit" class="btn btn-primary">Lưu Thay Đổi</button>
-        </form>
-    </div>
+<div class="container mt-5">
+    <h2 class="mb-4">Sửa thông tin học viên</h2>
+    <form action="index.php" method="POST" enctype="multipart/form-data">
+    <input type="hidden" name="action" value="xulysua">
+    <input type="hidden" name="id" value="<?php echo $h["id"]; ?>">
+      <div class="mb-3">
+        <label for="txtHoten" class="form-label">Họ và tên:</label>
+        <input type="text" class="form-control" id="txtHoten" name="hoten" required value="<?php echo $h["hoten"]; ?>">
+      </div>
+      <div class="mb-3">
+        <label for="txtNamsinh" class="form-label">Năm sinh:</label>
+        <input type="text" class="form-control" id="txtNamsinh" name="namsinh" required value="<?php echo $h["namsinh"]; ?>">
+      </div>
+      <div class="mb-3">
+        <label for="txtGioitinh" class="form-label">Giới tính:</label>
+        <select class="form-select" id="txtGioitinh" name="gioitinh" required>
+          <option value="Nam">Nam</option>
+          <option value="Nữ">Nữ</option>
+          <option value="Khác">Khác</option>
+        </select>
+      </div>
+      <div class="mb-3">
+        <label for="txtEmail" class="form-label">Email:</label>
+        <input type="email" class="form-control" id="txtEmail" name="email" required value="<?php echo $h["email"]; ?>">
+      </div>
+      <div class="mb-3">
+        <label for="txtSodienthoai" class="form-label">Số điện thoại:</label>
+        <input type="tel" class="form-control" id="txtSodienthoai" name="sodienthoai" required value="<?php echo $h["sodienthoai"]; ?>">
+      </div>
+      <div class="mb-3">
+        <label for="txtDiachi" class="form-label">Địa chỉ:</label>
+        <input type="text" class="form-control" id="txtDiachi" name="diachi" required value="<?php echo $h["diachi"]; ?>">
+      </div>
+      <div class="mb-3">
+        <label for="txtHinhanh" class="form-label">Hình ảnh:</label>
+        <input type="file" class="form-control" id="txtHinhanh" name="hinhanh" value="<?php echo $h["hinhanh"]; ?>">
+      </div>
+      <button type="submit" class="btn btn-primary">Lưu thay đổi</button>
+    </form>
+  </div>
+  <!-- Bootstrap JS -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-Lp7nqTi1Sqz9v4IovBfswM01R00DJqgG12NCgiiB11btq48Hp6KTlgLF0bPSWIqY" crossorigin="anonymous"></script>
 
-    <!-- Bootstrap JS và jQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 
 <?php include("../inc/bottom.php"); ?>
