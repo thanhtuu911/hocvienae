aea4b5595fa21d2c4a557f2d9e4f4995ea61a274
nguyenhoaithanh <nguyenhoaithanh.05042002@gmail.com>
2024-03-22 12:12:12 +0700

    thay đổi trang du học sinh với lãnh đạo

--- Public/home.php
+++ Public/home.php
@@ -32,102 +32,115 @@ include("inc/top.php");
     <div class="row">
         <!-- b1 -->
         <div class="col ">
-        <a href="#" class="btn" style="text-align: left; padding:0px; margin:0px;">
-        <div class="card">
-            <img src="../image/courses/B1.png" class="card-img-top" alt="B1"/>
-            
-            <div class="card-body">
-                <h5 class="card-title"> Du Học Mỹ</h5>
-                <p class="card-text">
-                "Mỹ có chất lượng giáo dục cao đi kèm với đó là hàng loạt các trường Đại học đứng đầu bảng xếp hạng thế giới như Đại học Harvard, Đại học Stanford,...
-                 Ngoài ra, Mỹ còn đào tạo đa dạng các ngành học với hệ thống kiến thức được kiểm duyệt vô cùng nghiêm ngặt.</p>
-                 <p> Hơn nữa, Mỹ có tổng 50 khu tiểu liên Bang và 1 đặc khu liên Bang (gồm 48 tiểu Bang lục địa), 
-                 cùng với vô số chủng tộc như người bản địa, người da màu, người châu Á định cư - tạo nên một nền văn hoá vô cùng đa dạng và phong phú. Khi sinh sống tại đây, bạn sẽ được tiếp xúc với nhiều nền văn hóa khác nhau,
-                 do đó, cải thiện được những kinh nghiệm sống, kiến thức thực tiễn và mở rộng vốn hiểu biết của bản thân"
-                </p>
-            </div>
+            <a href="#" class="btn" style="text-align: left; padding:0px; margin:0px;">
+                <div class="card">
+                    <img src="../image/courses/B1.png" class="card-img-top" alt="B1" />
 
-            <div class="card-footer">
-                <p class="card-text d-inline">
-                    Price: 
-                    <small>
-                        <del> 3000</del>
-                    </small>
-                    <span class="font-weight-bolder" ><i class="fa-solid fa-hand-holding-dollar"></i> 300 <span>
-                    </p>
-                    <a class="btn btn-primary text-white font-weight-bolder float-right" href="#"> Tham Gia</a>
-            </div>
-        </div>
-        </a>
+                    <div class="card-body">
+                        <h5 class="card-title"> Du Học Mỹ</h5>
+                        <p class="card-text">
+                            "Mỹ có chất lượng giáo dục cao đi kèm với đó là hàng loạt các trường Đại học đứng đầu bảng
+                            xếp hạng thế giới như Đại học Harvard, Đại học Stanford,...
+                            Ngoài ra, Mỹ còn đào tạo đa dạng các ngành học với hệ thống kiến thức được kiểm duyệt vô
+                            cùng nghiêm ngặt.</p>
+                        <p> Hơn nữa, Mỹ có nhiều tiểu bang, cùng với vô số chủng tộc như người bản địa, người da màu,
+                            người châu Á định cư - tạo nên một nền văn hoá vô cùng đa dạng và phong phú. Khi sinh sống
+                            tại đây, bạn sẽ được tiếp xúc với nhiều nền văn hóa khác nhau,
+                            do đó, cải thiện được những kinh nghiệm sống, kiến thức thực tiễn và mở rộng vốn hiểu biết
+                            của bản thân"
+                        </p>
+                    </div>
+
+                    <div class="card-footer">
+                        <p class="card-text d-inline"> Price:
+
+                            <small>
+                                <del> 3000</del>
+                            </small>
+                            <span class="font-weight-bolder"><i class="fa-solid fa-hand-holding-dollar"></i> 300<span>
+                        </p>
+                        <a class="btn btn-primary text-white font-weight-bolder float-right" href="#"> Tham Gia</a>
+                    </div>
+                </div>
+            </a>
         </div>
         <!-- b2 -->
         <div class="col ">
-        <a href="#" class="btn" style="text-align: left; padding:0px;">
-        <div class="card">
-            <img src="../image/courses/B2.png" class="card-img-top" alt="B2"/>
-            
-            <div class="card-body">
-                <h5 class="card-title"> Du học Australia</h5>
-                <p class="card-text">
-                "Du học Úc đã khẳng định vị thế của mình nhờ vào chất lượng giảng dạy, nghiên cứu chuyên sâu, và môi trường sinh sống,
-                 học tập lý tưởng. Úc đáp ứng nhu cầu của sinh viên quốc tế từ khắp nơi trên thế giới.</p>
-                <p> Sinh viên du học ở Úc cũng được trải nghiệm các nền văn hóa đa dạng và tiếp cận ý tưởng đổi mới. Hệ thống giáo dục Úc cung cấp tất
-                cả các cấp độ, với chính sách và pháp luật nhằm đảm bảo chất lượng giáo dục và bảo vệ 
-                quyền lợi của sinh viên quốc tế. Với danh tiếng quốc tế ấn tượng, sinh viên tốt nghiệp từ các trường Úc được săn đón trên toàn 
-                thế giới, do hệ thống giáo dục Úc được quản lý cẩn thận để duy trì các tiêu chuẩn cao và hình ảnh đất nước."
-                </p>
-            </div>
+            <a href="#" class="btn" style="text-align: left; padding:0px;">
+                <div class="card">
+                    <img src="../image/courses/B2.png" class="card-img-top" alt="B2" />
 
-            <div class="card-footer">
-                <p class="card-text d-inline">
-                    Price: 
-                    <small>
-                        <del > 4000</del>
-                    </small>
-                    <span class="font-weight-bolder" ><i class="fa-solid fa-hand-holding-dollar"></i> 400 <span>
-                    </p>
-                    <a class="btn btn-primary text-white font-weight-bolder " href="#">Tham Gia</a>
-            </div>
-        </div>
-    </a>
+                    <div class="card-body">
+                        <h5 class="card-title"> Du học Australia</h5>
+                        <p class="card-text">
+                            "Du học Úc đã khẳng định vị thế của mình nhờ vào chất lượng giảng dạy, nghiên cứu chuyên
+                            sâu, và môi trường sinh sống,
+                            học tập lý tưởng. Úc đáp ứng nhu cầu của sinh viên quốc tế từ khắp nơi trên thế giới.</p>
+                        <p> Sinh viên du học ở Úc cũng được trải nghiệm các nền văn hóa đa dạng và tiếp cận ý tưởng đổi
+                            mới. Hệ thống giáo dục Úc cung cấp tất
+                            cả các cấp độ, với chính sách và pháp luật nhằm đảm bảo chất lượng giáo dục và bảo vệ
+                            quyền lợi của sinh viên quốc tế. Với danh tiếng quốc tế ấn tượng, sinh viên tốt nghiệp từ
+                            các trường Úc được săn đón trên toàn
+                            thế giới, do hệ thống giáo dục Úc được quản lý cẩn thận để duy trì các tiêu chuẩn cao và
+                            hình ảnh đất nước."
+                        </p>
+                    </div>
+
+                    <div class="card-footer">
+                        <p class="card-text d-inline">
+                            Price:
+                            <small>
+                                <del> 4000</del>
+                            </small>
+                            <span class="font-weight-bolder"><i class="fa-solid fa-hand-holding-dollar"></i> 400 <span>
+                        </p>
+                        <a class="btn btn-primary text-white font-weight-bolder " href="#">Tham Gia</a>
+                    </div>
+                </div>
+            </a>
         </div>
         <div class="col ">
-        <a href="#" class="btn" style="text-align: left; padding:0px;">
-        <div class="card">
-            <img src="../image/courses/B2.png" class="card-img-top" alt="B2"/>
-            
-            <div class="card-body">
-                <h5 class="card-title"> Du Học Đức</h5>
-                <p class="card-text">
-                "Đức là một thiên đường giáo dục đại học. Không giống như ở bất kỳ quốc gia nào khác, du học Đức, bạn sẽ tìm thấy
-                 nhiều trường đại học được xếp hạng trên toàn thế giới, vô số khóa học để lựa chọn trong số đó, các bằng cấp có giá 
-                 trị toàn cầu hứa hẹn khả năng tuyển dụng cao và chi phí sinh hoạt hợp lý.</p>
+            <a href="#" class="btn" style="text-align: left; padding:0px;">
+                <div class="card">
+                    <img src="../image/courses/B2.png" class="card-img-top" alt="B2" />
 
-                <p> Với hơn 357.000 sinh viên quốc tế mong muốn lấy bằng đại học ở Đức, không có gì ngạc nhiên khi Đức được
-                 xếp hạng là một trong những điểm đến hàng đầu cho du học sinh trên thế giới. Mỗi năm, hàng ngàn học giả 
-                 từ mọi nơi tin tưởng học tập tại các trường đại học ở Đức, và lý do cho sự tin tưởng này rất rõ ràng."
-                </p>
-            </div>
+                    <div class="card-body">
+                        <h5 class="card-title"> Du Học Đức</h5>
+                        <p class="card-text">
+                            "Đức là một thiên đường giáo dục đại học. Không giống như ở bất kỳ quốc gia nào khác, du học
+                            Đức, bạn sẽ tìm thấy
+                            nhiều trường đại học được xếp hạng trên toàn thế giới, vô số khóa học để lựa chọn trong số
+                            đó, các bằng cấp có giá
+                            trị toàn cầu hứa hẹn khả năng tuyển dụng cao và chi phí sinh hoạt hợp lý.</p>
 
-            <div class="card-footer ">
-                <p class="card-text d-inline">
-                    Price: 
-                    <small>
-                        <del> 4000</del>
-                    </small>
-                    <span class="font-weight-bolder"><i class="fa-solid fa-hand-holding-dollar"></i> 400 <span>
-                    </p>
-                    <a class="btn btn-primary text-white font-weight-bolder " href="#">Tham Gia</a>
-            </div>
+                        <p> Với hơn 357.000 sinh viên quốc tế mong muốn lấy bằng đại học ở Đức, không có gì ngạc nhiên
+                            khi Đức được
+                            xếp hạng là một trong những điểm đến hàng đầu cho du học sinh trên thế giới. Mỗi năm, hàng
+                            ngàn học giả
+                            từ mọi nơi tin tưởng học tập tại các trường đại học ở Đức, và lý do cho sự tin tưởng này rất
+                            rõ ràng."
+                        </p>
+                    </div>
+
+                    <div class="card-footer ">
+                        <p class="card-text d-inline">
+                            Price:
+                            <small>
+                                <del> 4000</del>
+                            </small>
+                            <span class="font-weight-bolder"><i class="fa-solid fa-hand-holding-dollar"></i> 400 <span>
+                        </p>
+                        <a class="btn btn-primary text-white font-weight-bolder " href="#">Tham Gia</a>
+                    </div>
+                </div>
+            </a>
+
         </div>
-    </a>
-        
     </div>
-</div>
 
 
-<!-- Student -->
-<!-- <div class="container-fluid mt-5" style="background-color: #487289;" id="Feedback">
+    <!-- Student -->
+    <!-- <div class="container-fluid mt-5" style="background-color: #487289;" id="Feedback">
 <h1 class="text-center testyheading p-4"> Student Feedback</h1>
     <div class="row">
         <div class="col-md-12">
@@ -151,11 +164,8 @@ include("inc/top.php");
         </div>
     </div>
 </div> -->
-<!-- end Student -->
+    <!-- end Student -->
 
-
-
-
-<?php
+    <?php
 include("inc/bottom.php");
 ?>
--- Public/intro.php
+++ Public/intro.php
@@ -1,89 +1,99 @@
 <?php
 include("inc/top.php");
 ?>
-<div> <?php include("inc/carousel.php"); ?> </div>
-
-
-<div class=" container align-items-center px-4 px-lg-5  mt-5  p-5 ">
-    <h1 class='text-md-center p-3 mt-3'>Trung Tâm Ngoại Ngữ American English </h1>
+<div>
+    <!-- <?php include("inc/carousel.php"); ?> -->
 </div>
 
 
-<div class=" text-md-center">
-    <h5 class="text-md-center "><i> Trung Tâm Tư Vấn Du học – Ngoại ngữ AE. Môi trường Anh văn chuyên nghiệp, hiệu
-            quả.</i>
-        Với đội ngũ giảng viên, trợ giảng nhiệt tình, năng động, có trình độ chuyên môn cao, có kinh nghiệm trong quá
-        trình giảng dạy tiếng anh nhiều năm.
-        Đặc biệt với tinh thần lấy chữ “Tâm” đặt lên hàng đầu trong quá trình giảng dạy, trong công tác đào tạo thế hệ
-        trẻ.</i></h5>
-    <h1 class='m-3'>-------- <i class="fa-solid fa-graduation-cap"></i> -------- </h1>
-    </h3>
-</div>
-
-<style>
-.h1 {
-    color: #225470;
-}
-</style>
+<section class=''>
+    <div class="container align-items-center  mt-5 p-3 " style=" color:#225470">
+        <h1 class='text-md-center mt-5'>Trung Tâm Ngoại Ngữ American English </h1>
+    </div>
 
 
-<div class=" text-md-left m-5 p-3 ">
-    <p><span style="font-family: var( --e-global-typography-text-font-family ), Sans-serif; font-weight: 
+    <div class=" text-md-center m-4" style=" color:#225470">
+        <h5><i> Trung Tâm Tư Vấn Du học – Ngoại ngữ AE. Môi trường Anh văn chuyên nghiệp, hiệu quả.</i>     
+           <i>Với đội ngũ giảng viên, trợ giảng nhiệt tình, năng động,<br> có trình độ chuyên môn cao,
+            có kinh nghiệm trong quá trình giảng dạy tiếng anh nhiều năm.<br>  
+            Đặc biệt với tinh thần lấy chữ “Tâm” đặt lên hàng đầu trong quá trình giảng dạy, trong công tác đào tạo thế
+            hệ trẻ.</i>
+        </h5>
+           
+        <h1 class='m-3'>-------- <i class="fa-solid fa-graduation-cap"></i> -------- </h1>
+    </div>
+
+
+    <div class=" text-md-left m-5 p-3 ">
+        <p><span style="font-family: var( --e-global-typography-text-font-family ), Sans-serif; font-weight: 
     var( --e-global-typography-text-font-weight );">
-            Chương trình Ngoại ngữ AE &#8211; Khám phá thế giới với tiếng Anh!</span></p>
-    <p>Bạn đang tìm kiếm một chương trình đào tạo tiếng Anh chuyên nghiệp và đa dạng? Hãy cùng AE &#8211;
-        Trung tâm Ngoại ngữ hàng đầu, khám phá hành trình học tiếng Anh với một loạt các khóa học phù hợp với mọi độ
-        tuổi
-        và mục tiêu của bạn.</p>
-    <p><span style="font-family: var( --e-global-typography-text-font-family ), Sans-serif;">
-            Tại AE, chúng tôi tự hào mang đến cho bạn những lợi ích đặc biệt:</span></p>
-    <ul>
-        <li>Chuyên đào tạo từ cấp Tiểu học đến THCS và THPT: Với chương trình Tiếng Anh Trường học, chúng tôi tập trung
-            vào việc xây dựng nền
-            tảng vững chắc cho học sinh từ nhỏ, giúp các em phát triển kỹ năng ngôn ngữ và tự tin giao tiếp.</li>
-        <li>Tiếng Anh trẻ em và thanh thiếu niên: Chúng tôi hiểu rằng việc học ngôn ngữ cần được thú vị và phù hợp với
-            lứa tuổi. Với phương
-            giảng dạy tinh gọn, năng động và sử dụng tài liệu học tập phong phú,
-            chúng tôi giúp các em nhỏ và thanh thiếu niên phát triển khả năng ngôn ngữ một cách tự nhiên và hiệu quả.
-        </li>
-        <li>Tiếng Anh giao tiếp: Bạn muốn nắm vững kỹ năng giao tiếp tiếng Anh để tự tin trong công việc và cuộc sống
-            hàng ngày? Chương trình
-            Tiếng Anh Giao tiếp của chúng tôi sẽ giúp bạn phát triển khả năng diễn đạt, lắng nghe và hiểu rõ ngôn ngữ
-            trong môi trường giao
-            tiếp thực tế.</li>
-        <li>IELTS, PTE,&#8230;: Đối với những bạn có mục tiêu học tập hoặc làm việc ở nước ngoài, chúng tôi cung cấp các
-            khóa học chuẩn bị cho
-            các kỳ thi quốc tế như IELTS, PTE,&#8230;
-            Với phương pháp giảng dạy chuyên sâu và đội ngũ giáo viên giàu kinh nghiệm, chúng tôi cam kết giúp bạn đạt
-            được điểm số cao và thành
-            công trong hành trình của mình.</li>
-    </ul>
-    <p>&nbsp; &nbsp; &nbsp;AE là môi trường tiếng Anh 100%: Tại AE, bạn sẽ được tiếp xúc với môi trường tiếng Anh tuyệt
-        vời. Từ việc giao
-        tiếp hàng ngày với giáo viên
-        bản ngữ đến các hoạt động ngoại khóa và sự tương tác với các bạn học viên khác, bạn sẽ luôn bị &#8220;đắm
-        chìm&#8221;
-        trong ngôn ngữ Anh.</p>
-    <p><span style="font-family: var( --e-global-typography-text-font-family ), Sans-serif;">&nbsp; &nbsp; &nbsp;Phương
-            pháp
-            giảng dạy tinh gọn, hiện đại, hiệu quả, năng động: Tại AE,
-            chúng tôi sử dụng các phương pháp giảng dạy tiên tiến, tương tác và đáp ứng đa dạng nhu cầu học tập của các
-            học viên. Chúng tôi đảm
-            bảo rằng quá trình học diễn ra thú vị và mang lại kết quả cao.</span></p>
-    <p>&nbsp; &nbsp; &nbsp;Đội ngũ giáo viên người Bản xứ và Việt Nam đồng hành: Tại AE, chúng tôi sở hữu đội ngũ giáo
-        viên giàu kinh
-        nghiệm, được đào tạo chuyên sâu và đam mê giảng dạy. Sự kết hợp giữa giáo viên bản ngữ và giáo viên Việt Nam
-        giúp tạo nên môi trường
-        học tập đa dạng và phong phú.</p>
-    <p>&nbsp; &nbsp; &nbsp;Không gian đạt chuẩn, tiện nghi: Với không gian học đạt chuẩn, trang thiết bị hiện đại và
-        tiện nghi,
-        chúng tôi tạo ra một môi trường học tập tốt nhất cho bạn. Bạn sẽ cảm nhận được sự thoải mái và tiến bộ trong quá
-        trình học tại AE.</p>
-    <p>Hãy gia nhập AE ngay hôm nay và khám phá thế giới với tiếng Anh! Liên hệ với chúng tôi để biết thêm thông tin chi
-        tiết và nhận
-        tư vấn miễn phí.</p>
-</div>
-
+                Chương trình Ngoại ngữ AE &#8211; Khám phá thế giới với tiếng Anh!</span></p>
+        <p>Bạn đang tìm kiếm một chương trình đào tạo tiếng Anh chuyên nghiệp và đa dạng? Hãy cùng AE &#8211;
+            Trung tâm Ngoại ngữ hàng đầu, khám phá hành trình học tiếng Anh với một loạt các khóa học phù hợp với mọi độ
+            tuổi
+            và mục tiêu của bạn.</p>
+        <p><span style="font-family: var( --e-global-typography-text-font-family ), Sans-serif;">
+                Tại AE, chúng tôi tự hào mang đến cho bạn những lợi ích đặc biệt:</span></p>
+        <ul>
+            <li>Chuyên đào tạo từ cấp Tiểu học đến THCS và THPT: Với chương trình Tiếng Anh Trường học, chúng tôi tập
+                trung
+                vào việc xây dựng nền
+                tảng vững chắc cho học sinh từ nhỏ, giúp các em phát triển kỹ năng ngôn ngữ và tự tin giao tiếp.</li>
+            <li>Tiếng Anh trẻ em và thanh thiếu niên: Chúng tôi hiểu rằng việc học ngôn ngữ cần được thú vị và phù hợp
+                với
+                lứa tuổi. Với phương
+                giảng dạy tinh gọn, năng động và sử dụng tài liệu học tập phong phú,
+                chúng tôi giúp các em nhỏ và thanh thiếu niên phát triển khả năng ngôn ngữ một cách tự nhiên và hiệu
+                quả.
+            </li>
+            <li>Tiếng Anh giao tiếp: Bạn muốn nắm vững kỹ năng giao tiếp tiếng Anh để tự tin trong công việc và cuộc
+                sống
+                hàng ngày? Chương trình
+                Tiếng Anh Giao tiếp của chúng tôi sẽ giúp bạn phát triển khả năng diễn đạt, lắng nghe và hiểu rõ ngôn
+                ngữ
+                trong môi trường giao
+                tiếp thực tế.</li>
+            <li>IELTS, PTE,&#8230;: Đối với những bạn có mục tiêu học tập hoặc làm việc ở nước ngoài, chúng tôi cung cấp
+                các
+                khóa học chuẩn bị cho
+                các kỳ thi quốc tế như IELTS, PTE,&#8230;
+                Với phương pháp giảng dạy chuyên sâu và đội ngũ giáo viên giàu kinh nghiệm, chúng tôi cam kết giúp bạn
+                đạt
+                được điểm số cao và thành
+                công trong hành trình của mình.</li>
+        </ul>
+        <p>&nbsp; &nbsp; &nbsp;AE là môi trường tiếng Anh 100%: Tại AE, bạn sẽ được tiếp xúc với môi trường tiếng Anh
+            tuyệt
+            vời. Từ việc giao
+            tiếp hàng ngày với giáo viên
+            bản ngữ đến các hoạt động ngoại khóa và sự tương tác với các bạn học viên khác, bạn sẽ luôn bị &#8220;đắm
+            chìm&#8221;
+            trong ngôn ngữ Anh.</p>
+        <p><span style="font-family: var( --e-global-typography-text-font-family ), Sans-serif;">&nbsp; &nbsp;
+                &nbsp;Phương
+                pháp
+                giảng dạy tinh gọn, hiện đại, hiệu quả, năng động: Tại AE,
+                chúng tôi sử dụng các phương pháp giảng dạy tiên tiến, tương tác và đáp ứng đa dạng nhu cầu học tập của
+                các
+                học viên. Chúng tôi đảm
+                bảo rằng quá trình học diễn ra thú vị và mang lại kết quả cao.</span></p>
+        <p>&nbsp; &nbsp; &nbsp;Đội ngũ giáo viên người Bản xứ và Việt Nam đồng hành: Tại AE, chúng tôi sở hữu đội ngũ
+            giáo
+            viên giàu kinh
+            nghiệm, được đào tạo chuyên sâu và đam mê giảng dạy. Sự kết hợp giữa giáo viên bản ngữ và giáo viên Việt Nam
+            giúp tạo nên môi trường
+            học tập đa dạng và phong phú.</p>
+        <p>&nbsp; &nbsp; &nbsp;Không gian đạt chuẩn, tiện nghi: Với không gian học đạt chuẩn, trang thiết bị hiện đại và
+            tiện nghi,
+            chúng tôi tạo ra một môi trường học tập tốt nhất cho bạn. Bạn sẽ cảm nhận được sự thoải mái và tiến bộ trong
+            quá
+            trình học tại AE.</p>
+        <p>Hãy gia nhập AE ngay hôm nay và khám phá thế giới với tiếng Anh! Liên hệ với chúng tôi để biết thêm thông tin
+            chi
+            tiết và nhận
+            tư vấn miễn phí.</p>
+    </div>
+</section>
 <?php
 include("inc/bottom.php");
 ?>
